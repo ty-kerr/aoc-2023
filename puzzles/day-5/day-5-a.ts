b276ba846a3964ad3932cf21369a8d4e6e4e045f
tykerr <tykerr@gmail.com>
2024-04-18 15:23:45 -0700

    day 5

--- puzzles/day-5/day-5-a.ts
+++ puzzles/day-5/day-5-a.ts
@@ -1,10 +1,101 @@
-import { readData } from '../../shared.ts';
-import chalk from 'chalk';
+import { readData } from "../../shared.ts"
+import chalk from "chalk"
 
 export async function day5a(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+	const data = await readData(dataPath)
+
+	let seeds = data
+		.shift()!
+		.split(" ")
+		.slice(1)
+		.map((seed) => +seed)
+
+	type TMaps = {
+		[key: number]: {
+			dest: number[]
+			src: number[]
+			range: number[]
+		}
+	}
+
+	let maps: TMaps = {
+		1: {
+			dest: [],
+			src: [],
+			range: [],
+		},
+		2: {
+			dest: [],
+			src: [],
+			range: [],
+		},
+		3: {
+			dest: [],
+			src: [],
+			range: [],
+		},
+		4: {
+			dest: [],
+			src: [],
+			range: [],
+		},
+		5: {
+			dest: [],
+			src: [],
+			range: [],
+		},
+		6: {
+			dest: [],
+			src: [],
+			range: [],
+		},
+		7: {
+			dest: [],
+			src: [],
+			range: [],
+		},
+	}
+
+	let current = 0
+
+	for (let row of data) {
+		if (row.length) {
+			const splitted = row.split(" ")
+			if (isNaN(Number(splitted[0]))) {
+				current++
+			} else {
+				let dest = Number(splitted[0])
+				let src = Number(splitted[1])
+				let range = Number(splitted[2])
+				maps[current].dest.push(dest)
+				maps[current].src.push(src)
+				maps[current].range.push(range)
+			}
+		}
+	}
+
+	let result = Infinity
+	for (let seed of seeds) {
+		for (let i = 1; i <= 7; i++) {
+			let found = false
+			const { src, dest, range } = maps[i]
+
+			for (let j = 0; j < src.length; j++) {
+				if (found) {
+					break
+				}
+				if (seed >= src[j] && seed < src[j] + range[j]) {
+					seed = dest[j] + seed - src[j]
+					found = true
+				}
+			}
+		}
+
+		result = Math.min(result, seed)
+	}
+
+	return result
 }
 
-const answer = await day5a();
-console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
+const answer = await day5a()
+console.log(chalk.bgGreen("Your Answer:"), chalk.green(answer))
